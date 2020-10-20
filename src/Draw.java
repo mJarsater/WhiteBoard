64672cc18448868533513844f20c64c0aaa54f63
mjarsater <martin.jarsater@hotmail.com>
2020-10-20 14:02:59 +0200

    Kommentering klar

--- src/Draw.java
+++ src/Draw.java
@@ -1,9 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.*;
 import java.util.List;
 
@@ -109,7 +111,7 @@ class UDP extends Thread{
         start();
     }
 
-
+    /* start/run-metod som kör  */
     public void run(){
         try {
             // Socket för min port
@@ -124,31 +126,76 @@ class UDP extends Thread{
             //Loop för att fånga upp in inkommande data
             while(true){
                 datagramSocket.receive(incommingData);
+
+
                 byte [] data = incommingData.getData();
                 String incoming = new String(data);
                 String [] xy = incoming.trim().split(",");
+
+
                 Point p = new Point(Integer.parseInt(xy[0]), Integer.parseInt(xy[1]));
-                draw.addPoint(new Point(p));
+                draw.addPoint(p);
 
+
+                incommingData.setLength(buffer.length);
             }
-
         } catch (Exception e ){
             System.out.println("Fel i run.");
             System.out.println(e);
         }
     }
-
+    /*  Metod som kallar på andra metoder som krävs för sändning. */
     public void send(Point point){
         try {
-            String send = point.x + "," + point.y;
-            byte[] data = send.getBytes();
-            InetAddress toAdress = InetAddress.getByName(host);
-            DatagramPacket datagramPacket = new DatagramPacket(data, data.length, toAdress, toPort);
-            datagramSocket.send(datagramPacket);
+            byte[]data = getByteArray(point);
+
+            InetAddress toAddress = getAddress();
+
+            createDatagram(data, toAddress);
+
 
         } catch (Exception e ){
             System.out.println("Fel i send.");
             System.out.println(e);
+        }
+    }
+
+   /* Metod som hämtar x och y punkterna för en Point
+    *  och sparar de som bytes i en array. */
+    public byte[] getByteArray(Point p){
+        byte[] data = (Integer.toString(p.x) + " " +Integer.toString(p.y)).getBytes();
+        return data;
+    }
+
+    /* Hämtar adressen för hosten */
+    public InetAddress getAddress(){
+        InetAddress toAddress = null;
+        try {
+            toAddress = InetAddress.getByName(host);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return toAddress;
+    }
+
+    /* Metod som skapar en datagramsocket och packet. */
+    public void createDatagram(byte[]data, InetAddress toAddress){
+        try {
+            DatagramPacket datagramPacket = new DatagramPacket(data, data.length, toAddress, toPort);
+            DatagramSocket datagramSocket1 = new DatagramSocket();
+            sendDatagram(datagramSocket1, datagramPacket);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /* Metod som skickar datagrampakcet och sen stänger socketeN */
+    public void sendDatagram(DatagramSocket datagramSocket1, DatagramPacket datagramPacket){
+        try{
+            datagramSocket1.send(datagramPacket);
+            datagramSocket1.close();
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
